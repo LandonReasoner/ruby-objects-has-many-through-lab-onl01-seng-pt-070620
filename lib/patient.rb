726fbb7a920c6edb1260ee73c3c49c83436cf17d
Landon Reasoner <reasoner.landon90@gmail.com>
2020-08-13 01:02:11 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,21 @@
+class Patient 
+  
+  attr_accessor :name, :appointment, :doctor
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self 
+  end 
+  
+  def self.all
+    @@all
+  end
+  
+  def appointments
+    Appointment.all.select{|appointment| appointment.patient == self}
+  end
+  
+  
+end
